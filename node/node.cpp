90ff0d3823b1e3c0324a81dcd3e83188af03d0fd
Dev Bolton <jabolton939@gmail.com>
2026-08-15 15:37:48 +0000

    Implemented node connection and close on listening socket function

--- node/node.cpp
+++ node/node.cpp
@@ -8,9 +8,9 @@
 
 class Node{
     private:
-        int listen(){
-            int sockL = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-            if(sockL == -1){
+        int lconn(){
+            int sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if(sockfd == -1){
                 fprintf(stderr, "Failed to create socket\n");
                 return EXIT_FAILURE;
             }
@@ -20,7 +20,38 @@ class Node{
                 .sin_port = htons(8570), // Port number
                 .sin_addr.s_addr = htonl(INADDR_ANY) // Socket address (0.0.0.0, Any IPv4 address can connect)
             };
-            
+
+            if(bind(sockfd, (struct sockaddr*)&socketAddress, sizeof(socketAddress)) == -1){
+                fprintf(stderr, "Failed to bind socket to address\n");
+                close(sockfd);
+                return EXIT_FAILURE;
+            }
+
+            if(listen(sockfd, 16) == -1){
+                fprintf(stderr, "Socket failed to listen\n");
+                close(sockfd);
+                return EXIT_FAILURE;
+            }
+
+            while (true){
+                int connfd = accept(sockfd, NULL, NULL);
+                if (connfd == -1){
+                    fprintf(stderr, "Connection refused\n");
+                    close(sockfd);
+                    return EXIT_FAILURE;
+                }
+
+                // Connection logic
+
+                if(shutdown(connfd, SHUT_RDWR) == -1){
+                    fprintf(stderr, "Failed to shutdown connection\n");
+                    close(sockfd);
+                    close(connfd);
+                    return EXIT_FAILURE;
+                }
+                close(connfd);
+            }
+            close(sockfd);
         }
     public:
         Node(){}
