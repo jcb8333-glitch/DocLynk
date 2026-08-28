ea484826f09555b311c0c447c7e32d264e2175e2
dev bolton <jabolton939@gmail.com>
2026-08-28 21:44:30 +0000

    Undone docker compose ignore

--- src/node/bootnode_main.cpp
+++ src/node/bootnode_main.cpp
@@ -1,7 +1,18 @@
+#include <cstring>
 #include "bootnode.hpp"
 
-int main(){
-    const char* selfAddr = "";
-    BootNode boot(selfAddr);
-    boot.joinAll();
+int main(int argc, char* argv[]){
+    const char* selfAddr = nullptr;
+    for(int i = 1; i < argc-1; i++){
+        if(argv[i] == "--self"){
+            selfAddr = argv[i+1];
+        }
+    }
+    if(selfAddr){
+        BootNode boot(selfAddr);
+        boot.joinAll();
+    } else {
+        perror("Boot address is null");
+        return EXIT_FAILURE;
+    }
 }
--- src/node/node_main.cpp
+++ src/node/node_main.cpp
@@ -1,8 +1,24 @@
+#include <cstring>
 #include "node.hpp"
 
-int main(){
-    const char* selfAddr = "";
-    const char* bootAddr = "";
-    Node node(selfAddr, bootAddr);
-    node.joinAll();
+int main(int argc, char* argv[]){
+    const char* selfAddr = nullptr;
+    const char* bootAddr = nullptr;
+
+    for(int i = 1; i < argc-1; i++){
+        if(argv[i] == "--self"){
+            selfAddr = argv[i+1];
+        } 
+        else if(argv[i] == "--boot"){
+            bootAddr = argv[i+1];
+        }
+    }
+
+    if(selfAddr && bootAddr){
+        Node node(selfAddr, bootAddr);
+        node.joinAll();
+    } else {
+        perror("Node address or boot address is null");
+        return EXIT_FAILURE;
+    }
 }
