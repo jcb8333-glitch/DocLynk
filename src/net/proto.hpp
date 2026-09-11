eb2d4ed0440abbb17279086d30a13a27370dff01
dev bolton <jabolton939@gmail.com>
2026-09-11 16:45:49 +0000

    Fixed type include errors

--- src/net/proto.hpp
+++ src/net/proto.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
 enum class MsgType : uint8_t {
     RegReq = 0,
     RegRes = 1,
@@ -37,24 +41,4 @@ struct Packet {
 struct RouteEntry{
     uint64_t src;
     nInf node;
-};
-
-class PeerConn {
-    public:
-        int sockfd;
-        std::string peerAddr;
-        std::thread reader;
-        std::atomic<bool> alive{true};
-        std::mutex sendMutex;
-        std::mutex pendingMutex;
-        std::unordered_map<uint64_t, std::promise<Packet>> pending; 
-
-        PeerConn(int fd, std::string addr) : sockfd(fd), peerAddr(std::move(addr)){}
-
-        ~PeerConn(){
-            alive = false;
-            shutdown(sockfd, SHUT_RDWR);
-            close(sockfd);
-            if (reader.joinable()) reader.join();
-        }
 };
