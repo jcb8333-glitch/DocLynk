1794761d77585dafd69ce6ae42f00f6e8c3e40de
dev bolton <jabolton939@gmail.com>
2026-09-11 21:47:45 +0000

    Relocated send and recv packet functions

--- src/net/proto.hpp
+++ src/net/proto.hpp
@@ -17,7 +17,7 @@ struct nInf{
     uint64_t id;
     std::string addr;
     std::string targetAddr;
-    std::string secret;
+    std::vector<RouteEntry> routeTable;
     std::vector<nInf> connections;
 
     template <class Archive>
@@ -42,3 +42,28 @@ struct RouteEntry{
     uint64_t src;
     nInf node;
 };
+
+int sendPacket(int sockfd, Packet& packet){
+            std::stringstream ss;
+            {
+                cereal::BinaryOutputArchive archive(ss);
+                archive(packet);
+            }
+            std::string payload = ss.str();
+            uint32_t len = htonl(static_cast<uint32_t>(payload.size()));
+            if (send(sockfd, &len, sizeof(len), 0) != sizeof(len)) return -1;
+            if (send(sockfd, payload.data(), payload.size(), 0) != (ssize_t)payload.size()) return -2;
+            return 0;
+        }
+        // Receive serialized node
+        int recvPacket(int sockfd, Packet& packet){
+            uint32_t len;
+            if(recv(sockfd, &len, sizeof(len), MSG_WAITALL) != sizeof(len)) return -1;
+            len = ntohl(len);
+            std::string payload(len, '\0');
+            if (recv(sockfd, payload.data(), len, MSG_WAITALL) != (ssize_t)len) return -2;
+            std::stringstream ss(payload);
+            cereal::BinaryInputArchive archive(ss);
+            archive(packet);
+            return 0;
+        }
