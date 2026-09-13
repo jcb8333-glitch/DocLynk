d4d5d697c188a4e0eab8e5943a74809348a94d7b
dev bolton <jabolton939@gmail.com>
2026-09-13 23:37:42 +0000

    push prep

--- src/node/bootnode.hpp
+++ src/node/bootnode.hpp
@@ -11,16 +11,7 @@ class BootNode : public Node {
     private:
         NetStat net;
 
-        void handleConnection(int connfd, Packet& packet) override {
-            if(packet.type == MsgType::RtReq){
-                nInf newSuccessor = findSuccessor(packet.payload.id);
-                net.onDiscover(packet.payload);
-                Packet res{MsgType::RtRes, packet.packetID, packet.chordID, newSuccessor};
-                sendPacket(connfd, res);
-            } else {
-                Node::handleConnection(connfd, packet);
-            }
-        }
+        void handleConnection(int connfd, Packet& packet) override {}
 
         void route(){}
 
