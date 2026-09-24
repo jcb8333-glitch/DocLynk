6f8ea8cd05f1bac75dd6c098c8359bcd415be979
dev bolton <jabolton939@gmail.com>
2026-09-24 21:17:45 +0000

    Additions to vault header

--- src/block/state/vault.h
+++ src/block/state/vault.h
@@ -1,12 +1,29 @@
 #pragma once
 
 #include <string>
+#include <cstdint>
+#include <hash/sha1.hpp>
 
 /*
 Vault contains a hierarchical deterministic tree to hold tokens.
+Handles encryption and decryption of 
 A vault address is held by a token to know to who the token belongs.
 */
 class Vault{
+    public:
+        const uint64_t addr;
+
+        Vault();
+        
+        void sign(){
+            // On ownership authorized, vault encrypt transaction with public key
+        }
+        void accept(){
+            // On token receivec, decrypts token and sets token state to locked 
+            // This adds a transatction to mempool
+        }
     private:
-        std::string seed;
+        const std::string seed;
+        const int publicKey;
+        const int privateKey;
 };
